94d093c900ae94d718fa2223cf6a02413c786b58
Raul Moreno <raul.mor.1708@gmail.com>
2021-07-20 00:44:42 -0500

    Primer avance

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,163 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app class="main-view">
+    <v-app-bar app dense absolute color="deep-purple accent-4" dark shrink-on-scroll >
+      <v-toolbar-title>Dalasito Wiki</v-toolbar-title>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Personas</v-tab>
+          <v-tab>Animales</v-tab>
+          <v-tab>Objetos</v-tab>
+        </v-tabs>
+      </template>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <v-row justify="center">
+          <template v-for="(item, i) in items">
+            <v-col :key="i" cols="12" md="2" align="center">
+              <v-hover v-slot="{ hover }">
+                <v-card
+                  height="200px"
+                  width="200px"
+                  :elevation="hover ? 12 : 2"
+                  :class="{ 'on-hover': hover }"
+                >
+                  <v-img :src="item.img" height="200px" width="200px" @click="loadCharacterInfo(item.name)">
+                    <v-card-title class="text-h6 white--text">
+                      <v-row
+                        class="fill-height flex-column"
+                        justify="space-between"
+                      >
+                        <p class="mt-4 text-center text-border">
+                          {{ item.name }}
+                        </p>
+                      </v-row>
+                    </v-card-title>
+                  </v-img>
+                </v-card>
+              </v-hover>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
     </v-main>
+    <p align="center" class="footer">2021 Raúl Moreno</p>
+
+     <v-row justify="center" v-if="selectedCharacter != null" >
+      <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition" >
+        <v-card class="detail-view" >
+          <v-toolbar dark color="deep-purple accent-4">
+            <v-btn icon dark @click="dialog = false">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+            <v-toolbar-title>Detalles</v-toolbar-title>
+          </v-toolbar>
+
+          <v-list three-line subheader class="detail-view " align="center" >
+         
+          <v-list-item >
+            <v-list-item-content >
+              <v-list-item-title  class="white--text">Nombre</v-list-item-title>
+              <v-list-item-subtitle  class="white--text">{{selectedCharacter.name}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="white--text">Genero</v-list-item-title>
+              <v-list-item-subtitle class="white--text">{{selectedCharacter.gender}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="white--text">Fecha de nacimiento</v-list-item-title>
+              <v-list-item-subtitle class="white--text">{{selectedCharacter.born}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="white--text">Raza</v-list-item-title>
+              <v-list-item-subtitle class="white--text">{{selectedCharacter.race}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+           <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="white--text">Descripcion</v-list-item-title>
+              <p class="ps-12 pt-4 white--text">{{selectedCharacter.description}}</p>
+            </v-list-item-content>
+          </v-list-item>
+
+         
+
+        </v-list>
+        
+          
+        </v-card>
+      </v-dialog>
+    </v-row>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
+import {items} from './dataSource'
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  name: "App",
+  data() {
+    return {
+      items : [],
+      dialog: false,
+      selectedCharacter: null 
+    };
   },
-
-  data: () => ({
-    //
-  }),
+  mounted(){
+    this.getData()
+  },
+  methods:{
+    getData(){
+      this.items = items
+    },
+    loadCharacterInfo(name){
+      this.dialog = true
+      items.forEach(element => {
+        if (element.name == name){
+         var selectedCharacter = element
+         this.selectedCharacter = selectedCharacter
+        }
+        
+      });
+      
+    }
+  }
 };
 </script>
+
+<style>
+.v-card {
+  transition: opacity 0.3s ease-in-out;
+}
+
+.main-view{
+  background-color: rgb(61, 61, 61) !important; 
+}
+
+.detail-view{
+   background-color: rgb(61, 61, 61) !important; 
+}
+
+.text-border {
+  -webkit-text-stroke: 0.5px black !important;
+}
+
+.footer {
+  padding: 20px;
+  color: white;
+}
+
+
+</style>
---
+++ src/dataSource.js
@@ -0,0 +1,14 @@
+var items = [
+    {
+      name: "Dalas Review",
+      gender: "Hombre",
+      born: "31 de octubre de 1993 ",
+      race: "Humano",
+      description: "Daniel José Santomé Lemus (Santa Cruz de Tenerife, 31 de octubre de 1993), más conocido como Dalai Review, es un youtuber, influencer y escritor español. Nacido en Tenerife, en su juventud se trasladó a vivir a Barcelona para posteriormente trasladarse a Irlanda, que es donde vive actualmente. En el año 2012 creó su canal de Youtube por el que es conocido: «Dalas Review». En la actualidad, es uno de los YouTubers más conocidos de España con 9,9 millones de seguidores.",
+      type: "person",
+      img: "https://pbs.twimg.com/profile_images/1287431914450751489/fgKy6CCU_400x400.jpg",
+    },
+    
+    
+  ]
+export {items};
